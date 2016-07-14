920022e1aaf9fda0114aa95db608ec02e43f99b6
John Matthews <john@valdate.com>
2016-07-13 18:23:45 -0700

    Initial

---
+++ Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  AzureMatch
+//
+//  Created by John Matthews on 7/13/16.
+//  Copyright © 2016 SquirrelTime. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <MicrosoftAzureMobile/MicrosoftAzureMobile.h>
+
+#endif /* Bridging_Header_h */
